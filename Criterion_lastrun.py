a4d39de170b893c00b7778cfb82c5258c8137d7f
Yue Du <duyuescience@gmail.com>
2020-06-09 11:49:06 -0700

    lib folder

--- Criterion_lastrun.py
+++ Criterion_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.1.3),
-    on June 08, 2020, at 23:16
+    on June 09, 2020, at 11:45
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
